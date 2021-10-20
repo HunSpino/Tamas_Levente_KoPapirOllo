635cb7b1ed11dfa7f4e9de0297b15751374ad8e6
HunSpino <tamasleventelaszlo99@gmail.com>
2021-10-20 18:03:22 +0200

    init() + kep resize

--- app/src/main/java/com/petrik/kopapirollo/MainActivity.java
+++ app/src/main/java/com/petrik/kopapirollo/MainActivity.java
@@ -3,12 +3,37 @@ package com.petrik.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView tevalasztas,gepvalasztas;
+    private Button kobutton,papirbutton,ollobutton;
+    private ImageView[] kezjel;
+    private TextView eredmeny;
+
+
+    private Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+        kobutton=findViewById(R.id.ko_button);
+        papirbutton=findViewById(R.id.papir_button);
+        ollobutton=findViewById(R.id.ollo_button);
+        tevalasztas=findViewById(R.id.te_valasztas);
+        gepvalasztas=findViewById(R.id.gep_valasztas);
+        random=new Random();
+
     }
 }
